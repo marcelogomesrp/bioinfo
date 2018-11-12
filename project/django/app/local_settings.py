96fb88bcccb4e7ba35bb47dabc37c5e5de5f7419
Marcelo Gomes <marcelogomesrp@gmail.com>
2018-11-12 16:01:54 -0200

    Update local_settings.py

--- project/django/app/local_settings.py
+++ project/django/app/local_settings.py
@@ -4,8 +4,8 @@ DATABASES = {
 	'default': {
 		'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 		'NAME': 'bioinfo',						 # Or path to database file if using sqlite3.
-		'USER': 'bioinfo',						 # Not used with sqlite3.
-		'PASSWORD': 'SENHA',					 # Not used with sqlite3.
+		'USER': 'root',						 # Not used with sqlite3.
+		'PASSWORD': '',					 # Not used with sqlite3.
 		'HOST': '',						 # Set to empty string for localhost. Not used with sqlite3.
 		'PORT': '',						 # Set to empty string for default. Not used with sqlite3.
 	}
